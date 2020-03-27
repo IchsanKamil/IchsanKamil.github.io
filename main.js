34671b1e231f5b006986129b80caab4e2c41c60e
Muh Ichsan Kamil <muhichsank@gmail.com>
2020-03-28 06:03:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-let warna = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 165, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 128, 0)',
-    'rgb(0, 0, 255)',
-    'rgb(75, 0, 130)'
-];
+let warna = generateRandomColor(6);
+// try to input font awesome expression
 let cobaLagi = [
-    "Try again",
-    "Cmon",
-    "One more time",
-    "Arrgggghhhhhh",
-    "Ganbatte",
-    "coba lagi"
+    "Try again :)",
+    "C'mon XD",
+    "One more time :(",
+    "Arrgggghhhhhh +_+",
+    "Okay last -,-"
 ];
 let kotak = document.querySelectorAll(".kotak");
 let guess = document.querySelector("#guess");
-let chosen = warna[Math.floor(Math.random()*6)];
+let chosen = randomChosen();
 let alert1 = document.querySelector("#alert1");
 let h2 = document.querySelector("h2");
-// console.log(chosen);
+let button = document.querySelector("button");
+let x = 0;
+let h1h2 = document.getElementById("h1h2");
 
+// console.log(chosen);
 guess.textContent = chosen;
 
 for (let i = 0; i < kotak.length; i++) {
@@ -30,15 +26,19 @@ for (let i = 0; i < kotak.length; i++) {
         let click = this.style.backgroundColor;
         if (click === chosen) {
             // alert("True");
-            alert1.textContent = "Correct";
+            alert1.textContent = "Correct ^^";
             alert1.style.color = "green";
             correctColor(click);
-            h2.style.color = chosen;
+            h1h2.style.background = chosen;
+            x = 0
+            // h2.style.color = chosen;
+
         } else {
             // alert("False");
             this.style.backgroundColor = "rgb(4, 7, 47)";
-            alert1.textContent = cobaLagi[i];
+            alert1.textContent = cobaLagi[x];
             alert1.style.color = "red";
+            x++;
         }
     });
 }
@@ -48,3 +48,34 @@ function correctColor(chosen) {
         kotak[i].style.backgroundColor = chosen;
     }
 }
+function randomChosen() {
+    return warna[Math.floor(Math.random()*warna.length)];
+}
+function generateRandomColor(num) {
+    // add array
+    let arr = [];
+    // generate 256 colors
+    for (let i = 0; i < num; i++) {
+        let random = randomColor();
+        arr.push(random);
+        random = '';
+    }
+    return arr;
+}
+function randomColor() {
+    // "rgb(r, g, b)"
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+button.addEventListener("click", function() {
+    h1h2.style.background = "rgb(4, 7, 47)";
+    alert1.textContent = "";
+    warna = generateRandomColor(6);
+    chosen = randomChosen();
+    guess.textContent = chosen;
+    for (let i = 0; i < kotak.length; i++) {
+        kotak[i].style.backgroundColor = warna[i];
+    };
+});
